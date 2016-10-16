5f82149d8f7da812e8931ee57274fb8122490960
dany92 <dany@bc.edu>
2016-10-16 12:38:59 -0400

    axios successfully fetching the business info

--- src/client/app/routes/Home.js
+++ src/client/app/routes/Home.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Home extends Component {
+	constructor(props){
+		super(props);
+		this.state = { business: [] }
+	}
+	componentDidMount(){
+		axios.get('/api/business')
+		.then(business =>{
+			this.setState({business: business.data});
+			console.log(this.state.business);
+		})
+	}
 
 	render() {
 		return (

689fad1a66691357a391973271600a8b3bd3b22b
dany92 <dany@bc.edu>
2016-10-16 18:29:36 -0400

    before initializing redux

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 
 import Layout from "./components/Layout";
 import Home from "./routes/Home";
@@ -8,11 +10,17 @@ import Venue from "./routes/Venue";
 
 const app = document.getElementById('app');
 
-render(
-	<Router history={hashHistory}>
-		<Route path="/" component={ Layout }>
-			<IndexRoute component={ Home }></IndexRoute>
-			<Route path="venues" component={ Venue }></Route>
-		</Route>
-	</Router> 
-, app);
+const render = () => {
+	ReactDOM.render(
+		<Provider>
+			<Router history={hashHistory}>
+				<Route path="/" component={ Layout }>
+					<IndexRoute component={ Home }></IndexRoute>
+					<Route path="venues" component={ Venue }></Route>
+				</Route>
+			</Router>
+		</Provider> 
+	, app)
+}
+
+render();

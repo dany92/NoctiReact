992a4183f291f5b1a067fde816294b32dc59dab1
dany92 <dany@bc.edu>
2016-10-26 15:12:23 -0400

    event seeds added

---
+++ seed/event.js
@@ -0,0 +1,54 @@
+import db from '../server/db';
+const Event = db.model('event');
+
+
+const seedEvent = function () {
+
+    const events = [
+         {
+            title: 'Ladies Night',
+            description: 'The night is for ladies',
+            start_date: '2016-03-02 23:30',
+            end_date: '2016-03-03 4:30',
+            businessId: 1
+        },
+        {
+            title: 'Hot Summer Night',
+            description: 'Make your summer night memorable',
+            start_date: '2016-08-29 23:30',
+            end_date: '2016-08-30 4:30',
+            businessId: 1
+        },
+        {
+            title: 'Alpha Di Mixer',
+            description: 'Best Frat Party of the Year',
+            start_date: '2016-05-11 22:00',
+            end_date: '2016-05-12 2:00',
+            businessId: 1
+        },
+        {
+            title: 'Valentine Special',
+            description: "Couples, have night of fun. Singles, it's about time to meet someone",
+            start_date: '2016-02-14 23:00',
+            end_date: '2016-02-15 6:00',
+            businessId: 2
+        },
+        {
+            title: 'Christmas Event',
+            description: 'Party of the Year. Join us on White Christmas',
+            start_date: '2016-12-24 20:00',
+            end_date: '2016-12-25 4:00',
+            businessId: 3
+        }
+    ];
+
+
+    const creatingEvents = events.map(function (eventObj) {
+        return Event.create(eventObj);
+    });
+
+    return Promise.all(creatingEvents);
+
+};
+
+export default seedEvent;
--- seed/index.js
+++ seed/index.js
@@ -1,7 +1,8 @@
 import db from '../server/db';
 import venues from './venue';
+import events from './event';
 
-const seedPromises = [venues];
+const seedPromises = [venues, events];
 const seedDB = () => {
 	return Promise.all(seedPromises.map(seed => seed.call(this))) 
 	.then(() => {
